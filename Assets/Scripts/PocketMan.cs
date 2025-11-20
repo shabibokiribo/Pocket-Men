77a2c914fb1a752278a8c06b54028853e72f4ac5
shabibokiribo <Niahtaylor806@yahoo.com>
2025-11-20 18:51:44 -0500

    pocketmen

--- Assets/Scripts/PocketMan.cs
+++ Assets/Scripts/PocketMan.cs
@@ -5,8 +5,23 @@ public class PocketMan : ScriptableObject
 {
     public string manName;
     public int level;
-    public int maxHealth;
     public int attack;
     public int defense;
+    public int health;
     public Sprite sprite;
+
+    public string typeName;
+
+    [Header("Stat Ranges")]
+    public int minHealth;
+    public int maxHealth;
+
+    public int minAttack;
+    public int maxAttack;
+
+    public int minDefense;
+    public int maxDefense;
+
+    [Header("Possible Moves")]
+    public string[] possibleMoves;
 }
