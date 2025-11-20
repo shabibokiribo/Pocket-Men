f9f79350496e58dc5762c8d8c77034ad8661c8f3
shabibokiribo <Niahtaylor806@yahoo.com>
2025-11-19 22:46:04 -0500

    setup2
    mostly setting up player

---
+++ Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,24 @@
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    public float speed = 5.0f;
+    private Rigidbody2D rb;
+    public Vector2 input;
+
+    private void Awake()
+    {
+        rb = GetComponent<Rigidbody2D>(); //gets rigidbody from player object and assigns in script
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        input = new Vector2(Input.GetAxisRaw("Horizontal"), Input.GetAxisRaw("Vertical")); //gets the players input (horiuzontal is AD and the left and right arrows, etc.)
+    }
+
+    private void FixedUpdate()
+    {
+        rb.linearVelocity = input * speed;
+    }
+}
